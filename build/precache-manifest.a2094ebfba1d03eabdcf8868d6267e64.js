b252ec30d7d76e8a89d5e00674ea930d155d7429
lalitsharma <lalitsecret@gmail.com>
2021-06-14 12:47:42 +0530

    added build

---
+++ build/precache-manifest.a2094ebfba1d03eabdcf8868d6267e64.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce7b3987d6cef34c1753c4511c378fac",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1860573a159692ee557e",
+    "url": "/static/css/main.200a7cc1.chunk.css"
+  },
+  {
+    "revision": "6fa8d51bdcc6923d5bb3",
+    "url": "/static/js/2.3ea93f37.chunk.js"
+  },
+  {
+    "revision": "4ba1a1c11c57ed15aa68008707a3d6b4",
+    "url": "/static/js/2.3ea93f37.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1860573a159692ee557e",
+    "url": "/static/js/main.ab53cfce.chunk.js"
+  },
+  {
+    "revision": "98faa1f90ba787d3562e",
+    "url": "/static/js/runtime-main.203a6646.js"
+  }
+]);
